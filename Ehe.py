ba99390727330645744d75fd31c40c20e6e789ba
veronikadesnafau <veronikafau15@gmail.com>
2020-11-04 22:29:42 +0700

    https://github.com/muhammadarilda/Project-Praktikum.git

---
+++ Ehe.py
@@ -0,0 +1,16 @@
+print ("Hello")
+#mengambil input
+namalengkap = input ("nama lengkap: ")
+namapanggilan = input ("nama panggilan: ")
+nim = input ("nim:")
+tempatlahir = input ("tempat lahir:")
+umur = input ("umur")
+alamat = input ("alamat:")
+nohp = input ("no hp:")
+
+#menampilkan output
+print("", end='\n')
+print ("Assalamualaikum")
+print ("Let me introduce my self.","my name is",namalengkap,"but you can call me",namapanggilan,".My NIM is",nim, end='\n')
+print ("I was born in",tempatlahir,"and i am",umur,"years old", end='\n')
+print ("i am very glad if you want to invite my house in",alamat,".","So  don't fordget to call me before with the number",nohp)
